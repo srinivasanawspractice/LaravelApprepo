a25c0965564f8f7adb0a9904dc67550c84ed9e99
srinivasanawspractice <srinivasanbala0127@gmail.com>
2024-11-13 11:57:15 +0530

    Made script files executable and added again to deploy using ec2

---
+++ scripts/application_start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Starting the application"
+sudo systemctl restart httpd
+
+
---
+++ scripts/before_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Running BeforeInstall tasks"
+sudo chown -R ec2-user:ec2-user /var/www/uploadfileapp
+
+
