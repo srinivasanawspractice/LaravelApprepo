e41dcb1391a964890a8eea35c2ef0d04fb7e85a5
devopsincupent <devops@cupent.com>
2025-01-02 16:22:28 +0530

    new script code

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Check if the uploadfileapp directory exists
+if [ ! -d "/var/www/uploadfileapp" ]; then
+    # If the directory does not exist, create it
+    echo "Creating /var/www/uploadfileapp directory"
+    sudo mkdir -p /var/www/uploadfileapp
+else
+    echo "/var/www/uploadfileapp already exists"
+fi
+
+# Proceed with the rest of the tasks
 echo "Running BeforeInstall tasks"
 sudo chown -R ec2-user:ec2-user /var/www/uploadfileapp
 
